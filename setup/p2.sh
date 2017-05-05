24b030eb9c5268e9dd7ecccc101dd1f29ae89cc2
Dymshits <mdymshits@paypal.com>
2017-05-05 17:05:11 +0300

    {t2, p2}.sh are added

---
+++ setup/p2.sh
@@ -0,0 +1,9 @@
+alias aws-get-p2=export instanceId=`aws ec2 describe-instances --filters "Name=instance-state-name,Values=stopped,Name=instance-type,Values=p2.xlarge" --query "Reservations[0].Instances[0].InstanceId"` && echo $instanceId
+
+wait ${!}
+
+alias aws-start=aws ec2 start-instances --instance-ids $instanceId && aws ec2 wait instance-running --instance-ids $instanceId && export instanceIp=`aws ec2 describe-instances --filters "Name=instance-id,Values=$instanceId" --query "Reservations[0].Instances[0].PublicIpAddress"` && echo $instanceIp
+
+wait ${!}
+
+alias aws-ssh=ssh -i ~/.ssh/aws-key-fast-ai.pem ubuntu@$instanceIp
