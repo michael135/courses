629adb1e811fccb4da56d8b4bdd2ce81021c2bfb
Dymshits <mdymshits@paypal.com>
2017-05-05 17:19:04 +0300

    repair t2, p2, aws-alias

--- setup/t2.sh
+++ setup/t2.sh
@@ -1,12 +1,12 @@
-alias aws-get-t2=export instanceId=`aws ec2 describe-instances --filters "Name=instance-state-name,Values=stopped,Name=instance-type,Values=t2.xlarge" --query "Reservations[0].Instances[0].InstanceId"` && echo $instanceId
+export instanceId=`aws ec2 describe-instances --filters "Name=instance-state-name,Values=stopped,Name=instance-type,Values=t2.large" --query "Reservations[0].Instances[0].InstanceId"` && echo $instanceId
 
 wait ${!}
 
-alias aws-start=aws ec2 start-instances --instance-ids $instanceId && aws ec2 wait instance-running --instance-ids $instanceId && export instanceIp=`aws ec2 describe-instances --filters "Name=instance-id,Values=$instanceId" --query "Reservations[0].Instances[0].PublicIpAddress"` && echo $instanceIp
+aws ec2 start-instances --instance-ids $instanceId && aws ec2 wait instance-running --instance-ids $instanceId && export instanceIp=`aws ec2 describe-instances --filters "Name=instance-id,Values=$instanceId" --query "Reservations[0].Instances[0].PublicIpAddress"` && echo $instanceIp
 
 wait ${!}
 
-alias aws-ssh=ssh -i ~/.ssh/aws-key-fast-ai.pem ubuntu@$instanceIp
+ssh -i ~/.ssh/aws-key-fast-ai.pem ubuntu@$instanceIp
 
 
 
